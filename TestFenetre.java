73f54eea1712cd7ad9093bbc298303180f612145
Needs <needs@mailoo.org>
2014-03-14 16:37:31 +0100

    Ajout des conseils du jour.

--- TestFenetre.java
+++ TestFenetre.java
@@ -1,52 +1,119 @@
+import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
 
-public class TestFenetre implements Runnable {
+public class TestFenetre implements Runnable, ActionListener, MenuListener {
+	private JFrame frame;
+
+	public void show_about() {
+		JOptionPane.showMessageDialog(frame,
+					      "PaintFactoryManager\n" +
+					      "A new kind of PhotoshopStrategyListener (Approved by test driven Dev.)\n\n" +
+					      "By the mysterious team Jeretim.");
+	}
+
+	public void show_tipofday() {
+		int n = (new Random()).nextInt(3);
+
+		if (n == 0) {
+			Object[] choices = {"La pizza 4 fromages", "La nullPointerException", "La réponse D"};
+			String s = (String)JOptionPane.showInputDialog(
+				frame,
+				"C'est l'heure de ...",
+				"Conseil du jour",
+				JOptionPane.PLAIN_MESSAGE,
+				null,
+				choices,
+				null);
+			if (s == choices[1]) {
+				try {
+					throw null;
+				} catch(Exception e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
+		} else if (n == 1) {
+			JOptionPane.showMessageDialog(
+				frame, "Les conseils du jour son accéssible dans le menu Aide -> Conseil du jour");
+		} else if (n == 2) {
+			String s = (String)JOptionPane.showInputDialog(
+				frame,
+				"Pour plus de sécurité, PaintFactoryManager vous conseil de rentrer votre mot de passe ci-dessous",
+				"Conseil du jour",
+				JOptionPane.PLAIN_MESSAGE,
+				null,
+				null,
+				null);
+		}
+	}
+
 	public JMenuBar create_menubar() {
 		JMenuBar bar = new JMenuBar();
-		JMenu fichier = new JMenu("Fichier");
-		JMenu edition = new JMenu("Edition");
+		JMenu file   = new JMenu("Fichier");
+		JMenu edit   = new JMenu("Edition");
+		JMenu help   = new JMenu("Aide");
 		JMenuItem item;
 
 		// Fichier
-		fichier.setMnemonic(KeyEvent.VK_F);
+		file.setMnemonic(KeyEvent.VK_F);
 		item = new JMenuItem("Nouveau",
 				     UIManager.getIcon("FileView.fileIcon"));
 		item.setMnemonic(KeyEvent.VK_N);
-		fichier.add(item);
+		file.add(item);
 		item = new JMenuItem("Ouvrir",
 				     UIManager.getIcon("FileView.directoryIcon"));
 		item.setMnemonic(KeyEvent.VK_O);
-		fichier.add(item);
+		file.add(item);
 		item = new JMenuItem("Enregistrer",
 				     UIManager.getIcon("FileView.floppyDriveIcon"));
 		item.setMnemonic(KeyEvent.VK_S);
-		fichier.add(item);
-		fichier.addSeparator();
+		file.add(item);
+		file.addSeparator();
 		item = new JMenuItem("Quitter", KeyEvent.VK_Q);
-		fichier.add(item);
+		file.add(item);
 
 		// Edition
-		edition.setMnemonic(KeyEvent.VK_E);
+		edit.setMnemonic(KeyEvent.VK_E);
 		item = new JMenuItem("Annuler", KeyEvent.VK_A);
-		edition.add(item);
+		edit.add(item);
 		item = new JMenuItem("Refaire", KeyEvent.VK_R);
-		edition.add(item);
-		edition.addSeparator();
+		edit.add(item);
+		edit.addSeparator();
 		item = new JMenuItem("Copier", KeyEvent.VK_C);
-		edition.add(item);
+		edit.add(item);
 		item = new JMenuItem("Coller", KeyEvent.VK_V);
-		edition.add(item);
+		edit.add(item);
 
-		bar.add(fichier);
-		bar.add(edition);
+		// Aide
+		help.setMnemonic(KeyEvent.VK_H);
+		item = new JMenuItem("Conseil du jour");
+		item.setActionCommand("tipofday");
+		item.addActionListener(this);
+		help.add(item);
+		item = new JMenuItem("Apropos",
+				     UIManager.getIcon("OptionPane.informationIcon"));
+		item.setMnemonic(KeyEvent.VK_A);
+		item.setActionCommand("about");
+		item.addActionListener(this);
+		help.add(item);
+		item = new JMenuItem("Aide",
+				     UIManager.getIcon("OptionPane.questionIcon"));
+		item.setMnemonic(KeyEvent.VK_H);
+		help.addSeparator();
+		help.add(item);
+
+		bar.add(file);
+		bar.add(edit);
+		bar.add(help);
 
 		return bar;
 	}
 
-	public JFrame create_frame() {
-		JFrame frame = new JFrame("PaintFactoryManager");
+	public void create_frame() {
+		frame = new JFrame("PaintFactoryManager");
 		AireDeDessin drawarea = new AireDeDessin(1000, 1000);
 		EcouteurDevenements listener = new EcouteurDevenements(drawarea);
 
@@ -56,11 +123,10 @@ public class TestFenetre implements Runnable {
 
 		frame.add(drawarea);
 		frame.setJMenuBar(create_menubar());
-		return frame;
 	}
 
 	public void run() {
-		JFrame frame = create_frame();
+		create_frame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500, 200);
 		frame.setVisible(true);
@@ -69,4 +135,22 @@ public class TestFenetre implements Runnable {
 	public static void main(String [] args) {
 		SwingUtilities.invokeLater(new TestFenetre());
 	}
+
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+
+		if (command == "about") {
+			show_about();
+		} else if (command == "tipofday") {
+			show_tipofday();
+		}
+	}
+
+	public void menuSelected(MenuEvent e) {
+		show_about();
+	}
+
+	// Ignore les auters evenement de MenuListener
+	public void menuDeselected(MenuEvent e) {}
+	public void menuCanceled(MenuEvent e) {}
 }

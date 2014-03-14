d924c344e529acb5b51891cd0709716649b6879d
derdaelj <jeremy.derdaele@gmail.com>
2014-03-14 17:09:05 +0100

    Tools tweak and corrections

--- com/jeretim/PaintFactoryManager/Tool/Eraser.java
+++ com/jeretim/PaintFactoryManager/Tool/Eraser.java
@@ -2,6 +2,7 @@ package com.jeretim.PaintFactoryManager.Tool;
 
 import java.awt.*;
 import java.awt.image.*;
+import java.awt.geom.Line2D;
 
 public class Eraser extends Tool
 {
@@ -12,6 +13,11 @@ public class Eraser extends Tool
 	public void trace(BufferedImage img, Point src, Point dest) {
 		Graphics2D drawable = img.createGraphics();
 		drawable.setPaint(Color.white);
-		drawable.fillRect(src.x - radius/2, src.y - radius/2, radius, radius);
+		drawable.setStroke(new BasicStroke(radius));
+		drawable.draw(new Line2D.Float(src.x, src.y, dest.x, dest.y));
+	}
+
+	public Tool clone() {
+		return new Eraser(radius);
 	}
 }
